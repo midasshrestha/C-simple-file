f1988c4d0a35bb15975348d37e975241ac2bae92
midasshrestha <midas.shrestha01@gmail.com>
2019-11-22 18:20:03 -0600

    simple syntax

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum;
+            myNum = 22;
+            Console.WriteLine(myNum);
+        }
+    }
+}
+
